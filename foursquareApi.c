c0cd035f8d959b3b22f02dae87ceebb537367432
Aditya Tiwari <500067156@stu.upes.ac.in>
2020-09-28 11:47:09 +0530

    Update foursquareApi.c

--- foursquareApi.c
+++ foursquareApi.c
@@ -41,48 +41,23 @@ int main(void)
   chunk.size = 0;    /* no data at this point */ 
  
   curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* init the curl session */ 
   curl_handle = curl_easy_init();
- 
-  /* specify URL to get */ 
   curl_easy_setopt(curl_handle, CURLOPT_URL, "https://api.foursquare.com/v2/venues/trending?client_id=ODFO3RLC1RT3MSJHH03G0G1AEYUYVIMRHTTJIHZWU10T42KV&client_secret=TQ3IL2OVKS0LO4BDMAG13TKUU0WKFSMDXFYMVKIXKHFDFCLO&v=20180323&ll=19.0760,-72.8777&query=coffee&limit=5");
- 
-  /* send all data to this function  */ 
   curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
- 
-  /* we pass our 'chunk' struct to the callback function */ 
   curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
- 
-  /* some servers don't like requests that are made without a user-agent
-     field, so we provide one */ 
   curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
- 
-  /* get it! */ 
   res = curl_easy_perform(curl_handle);
- 
-  /* check for errors */ 
+
   if(res != CURLE_OK) {
     fprintf(stderr, "curl_easy_perform() failed: %s\n",
             curl_easy_strerror(res));
   }
   else {
-    /*
-     * Now, our chunk.memory points to a memory block that is chunk.size
-     * bytes big and contains the remote file.
-     *
-     * Do something nice with it!
-     */ 
- 
     printf("%s bytes retrieved\n", chunk.memory);
   }
- 
-  /* cleanup curl stuff */ 
   curl_easy_cleanup(curl_handle);
  
   free(chunk.memory);
- 
-  /* we're done with libcurl, so clean it up */ 
   curl_global_cleanup();
  
   return 0;
